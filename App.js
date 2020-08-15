bf5cc9e3a35c6e9197bc0b34f489723fbb698582
Gianfranco Manganiello <gianfrancomanganiello1997@gmail.com>
2020-08-16 01:24:29 +0200

    feat: create button to change theme
    Signed-off-by: Gianfranco Manganiello <gianfrancomanganiello1997@gmail.com>

--- App.js
+++ App.js
@@ -1,20 +1,36 @@
 import React from 'react';
 
 import * as eva from '@eva-design/eva';
-import {ApplicationProvider, IconRegistry, Layout, Text} from '@ui-kitten/components';
+import {
+  ApplicationProvider,
+  IconRegistry,
+  Toggle,
+  Layout,
+} from '@ui-kitten/components';
 import {NavigationContainer} from '@react-navigation/native';
 import {EvaIconsPack} from '@ui-kitten/eva-icons';
 
 import AppNavigator from './src/navigation';
 
 const App = () => {
+  const [checked, setChecked] = React.useState(false);
+
+  const onCheckedChange = (isChecked) => {
+    setChecked(isChecked);
+  };
+
   return (
     <>
       <IconRegistry icons={EvaIconsPack} />
-      <ApplicationProvider {...eva} theme={eva.light}>
-        <NavigationContainer>
-          <AppNavigator />
-        </NavigationContainer>
+      <ApplicationProvider {...eva} theme={checked ? eva.light : eva.dark}>
+        <Layout style={{flex: 1}}>
+          <NavigationContainer>
+            <AppNavigator />
+          </NavigationContainer>
+          <Toggle style={{marginBottom: 36, padding: 20}} checked={checked} onChange={onCheckedChange}>
+            {`Mode: ${checked ? 'Light' : 'Dark'}`}
+          </Toggle>
+        </Layout>
       </ApplicationProvider>
     </>
   );
